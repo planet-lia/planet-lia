6283a061963eaacceb8a8ea68681fd3589559dcb
svigelj <aljaz.svigelj@gmail.com>
2019-09-15 14:48:41 +0200

    chore(match-generator-base): base project with simple README.md

---
+++ game-utils/match-generator-base/desktop/src/com/planet_lia/match_generator_base/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.planet_lia.match_generator_base.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.planet_lia.match_generator_base.MatchGeneratorBase;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MatchGeneratorBase(), config);
+	}
+}
