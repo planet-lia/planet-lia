42ece59375f2ca553902c03acbc8ed651dd6629d
greenstatic <gregor@krmelj.xyz>
2019-10-11 11:38:35 +0200

    refactor(match-viewer): add argparse to serve_games_assets.py script

--- game-utils/match-viewer/serve_games_assets.py
+++ game-utils/match-viewer/serve_games_assets.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
-# Usage: python cors_http_server.py <dir> <port>
 
-import sys
+import argparse
 import os
-os.chdir(sys.argv[1])
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", help="Root dir for the HTTP server", nargs=1)
+parser.add_argument("-p", "--port", type=int, help="Port to bind the HTTP server", required=False, default=3333)
+parser.add_argument("-b", "--bind", help="Bind address of HTTP server", required=False, default="127.0.0.1")
+cli_args = parser.parse_args()
+
+os.chdir(cli_args.dir[0])
 
 try:
     # try to use Python 3
     from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
     def test (*args):
-        test_orig(*args, port=int(sys.argv[2]))
+        test_orig(*args, bind=cli_args.bind, port=cli_args.port)
 except ImportError: 
 	  # fall back to Python 2
     from BaseHTTPServer import HTTPServer, test
     from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
+class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
